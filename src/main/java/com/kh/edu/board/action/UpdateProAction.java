c480ca5e7d30a087f0cf7c5fc186d63daebd9d57
kyuhon <kyuhon123@gmail.com>
2024-12-19 18:44:26 +0900

    [create] 추가 코딩

--- src/main/java/com/kh/edu/board/action/UpdateProAction.java
+++ src/main/java/com/kh/edu/board/action/UpdateProAction.java
@@ -1,6 +1,8 @@
 package com.kh.edu.board.action;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,19 +15,19 @@ public class UpdateProAction implements CommandAction {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		  String pageNum = request.getParameter("pageNum");
-		  BoardVO article = new BoardVO();
-		         article.setNum(Integer.parseInt(request.getParameter("num")));
-		         article.setWriter(request.getParameter("writer"));
-		         article.setEmail(request.getParameter("email"));
-		         article.setSubject(request.getParameter("subject"));
-		         article.setContent(request.getParameter("content"));
-		         article.setPass(request.getParameter("pass"));
-		  BoardDAO dbPro = BoardDAO.getInstance();
-		         int check = dbPro.updateDB(article);
-		         request.setAttribute("pageNum", new Integer(pageNum));
-		         request.setAttribute("check", new Integer(check));
-		         return new ActionForward("/board/updatePro.jsp", false);
+		request.setCharacterEncoding("UTF-8");
+		String pageNum = request.getParameter("pageNum");
+		BoardVO article = new BoardVO();
+		article.setNum(Integer.parseInt(request.getParameter("num")));
+		article.setWriter(request.getParameter("writer"));
+		article.setEmail(request.getParameter("email"));
+		article.setSubject(request.getParameter("subject"));
+		article.setContent(request.getParameter("content"));
+		article.setPass(request.getParameter("pass"));
+		BoardDAO dbPro = BoardDAO.getInstance();
+		int check = dbPro.updateDB(article);
+		request.setAttribute("pageNum", pageNum);
+		request.setAttribute("check", check);
+		return new ActionForward("/board/updatePro.jsp", false);
 	}
-
 }
